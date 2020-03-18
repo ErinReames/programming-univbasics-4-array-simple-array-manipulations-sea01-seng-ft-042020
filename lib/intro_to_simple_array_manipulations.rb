da2220d68a1aeda07cf16aea1d439f2a8b15a764
Erin Reames <italiagirl83@gmail.com>
2020-03-18 14:49:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,3 +27,17 @@ end
 def using_insert(array, element)
   
 end
+
+def using_uniq(array)
+  array.using_uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+end
+
+def using_delete_at(array, integer)
+end
